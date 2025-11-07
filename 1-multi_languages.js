4a93f14defeebaf4f0d1e2cccb243dfddd4a53e7
the-blockman <105639638+the-blockman@users.noreply.github.com>
2025-11-07 18:18:00 +0100

    Add multi-language messages to console output

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+let firstMessage = "C is fun";
+let secondMessage = "Python is cool";
+let thirdMessage = "Javascript is amazing";
+
+console.log(firstMessage);
+console.log(secondMessage);
+console.log(thirdMessage);
