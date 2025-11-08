0250344abbddda65c2cb4e0c9c6d31c13267854f
the-blockman <105639638+the-blockman@users.noreply.github.com>
2025-11-08 21:41:38 +0100

    Refactor argument handling to simplify logic for displaying command line input

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,7 @@
 const { argv } = require("node:process");
 
-let totalArrayIndex = argv.length - 1;
-
-if (totalArrayIndex <= 1) {
-  console.log("No argument");
-} else {
+if (argv[2]) {
   console.log(argv[2]);
+} else {
+  console.log("No argument");
 }
