a10d3ae4fb0235577b76a76d867d01fe39aca263
the-blockman <105639638+the-blockman@users.noreply.github.com>
2025-11-09 17:19:50 +0100

    Refactor factorial function to use Big.js for accurate large number calculations

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,18 +1,18 @@
-const { Console } = require("node:console");
 const { argv } = require("node:process");
+const Big = require("big.js");
 
 let integer = parseInt(argv[2]);
 
 function factorial(a) {
-  let result = 1;
+  let result = Big(1);
   for (i = a; i >= 1; i--) {
-    result *= i;
+    result = result.times(i);
   }
   return result;
 }
 
-if (integer === NaN) {
+if (isNaN(integer)) {
   console.log(1);
 } else {
-  console.log(factorial(integer));
+  console.log(factorial(integer).toString());
 }
